1bfddac7776a2633924c8ca4079c66198887ec08
Jannes Kretschmer <jannes.kretschmer@gmx.de>
2021-05-29 18:14:45 +0200

    initial commit

--- src/test/java/jw/notify/JWNotificationTriggerTest.java
+++ src/test/java/jw/notify/JWNotificationTriggerTest.java
@@ -1,7 +1,5 @@
 package jw.notify;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.IOException;
 
 import javax.mail.MessagingException;
@@ -12,13 +10,7 @@ import org.junit.jupiter.api.Test;
 public class JWNotificationTriggerTest {
 
 	@Test
-	public void hello() {
-		System.out.println("lol");
-		assertTrue(1 == 2 - 1);
-	}
-
-	@Test
-	public void trigger() throws AddressException, InterruptedException, IOException, MessagingException {
+	public void testTrigger() throws AddressException, InterruptedException, IOException, MessagingException {
 		Main.main(null);
 	}
 
